b84b78bed7e89f707509dd91c67fbade8a8b96fb
Sergij Pankiv <pankivsergij97@gmail.com>
2023-01-13 20:25:25 +0200

    http request was added

--- app.js
+++ app.js
@@ -10,9 +10,13 @@
 //Number: myNumber --> Number.prototype --> Object.prototype. --> null
 //Boolean: myBoolean --> Boolean.prototype --> Object.prototype. --> null
 
+//HTTP(Hypertext Transfer Protocol)
+//request - what we want to do
+//Response - what was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cater', 5)
+const game1 = new Hangman('Request', 5)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
@@ -25,3 +29,42 @@ window.addEventListener('keypress', function(e) {
 	puzzleEl.textContent = game1.puzzle
 	guessesEl.textContent = game1.statusMessage
 })
+
+// Making http request 
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState === 4 && e.target.status === 200) {
+		const data = JSON.parse(e.target.responseText)
+		console.log(data)
+	} else if (e.target.readyState === 4) {
+		console.log('Unable to fetch data')
+	}
+})
+
+// request.open('GET', 'https://puzzle.mead.io/puzzle')
+// request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
+
+// 1 Make a new request for all countries
+// 2 Parse the responseText
+// 3 Find your country object
+// 4 Print the full country name
+
+const countryCode = 'UA'
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+	
+	if(e.target.readyState === 4 && e.target.status === 200){
+
+		const data = JSON.parse(e.target.responseText)
+		const country = data.find((country) => country.cca2 === countryCode)
+			console.log(country.name.common)
+	} else if (e.target.readyState === 4) {
+			console.log('Unable to fetch data')
+	}
+
+})
+
+countryRequest.open('GET', 'https://restcountries.com/v3.1/all')
+countryRequest.send()
