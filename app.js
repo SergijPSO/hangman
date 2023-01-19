27b7f21c967252081dcefa6b87b782a781a6394c
Sergij Pankiv <pankivsergij97@gmail.com>
2023-01-19 21:48:40 +0200

    promises was integrated

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const game1 = new Hangman('Request', 5)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
+
 console.log(game1.statusMessage)
 console.log(game1.gameStatus)
 
@@ -30,49 +31,14 @@ window.addEventListener('keypress', function(e) {
 	guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle('2',(error, puzzle) => {
-	if(error) {
-		console.log(`Error is ${error}}`)
-	} else {
-		console.log(puzzle)
-	}
+getPuzzle('2').then((puzzle) => {
+	console.log(puzzle)
+}, (err) => {
+	console.log(`Error: ${err}`)
 })
 
-getCountry('UA', (error, country) => {
-	if(error) {
-		console.log(error)
-	} else {
-		console.log(`Country name: ${country.name.common}`)
-	}
+getCountry('UA').then((country) => {
+	console.log(`Country name: ${country.name.common}`)
+}, (error) => {
+	console.log(`Error: ${error}`)
 })
-
-// Making http request 
-// const request = new XMLHttpRequest()
-// request.addEventListener('readystatechange', (e) => {
-// 	if (e.target.readyState === 4 && e.target.status === 200) {
-// 		const data = JSON.parse(e.target.responseText)
-// 		console.log(data)
-// 	} else if (e.target.readyState === 4) {
-// 		console.log('Unable to fetch data')
-// 	}
-// })
-
-
-// const countryCode = 'UA'
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-	
-// 	if(e.target.readyState === 4 && e.target.status === 200){
-
-// 		const data = JSON.parse(e.target.responseText)
-// 		const country = data.find((country) => country.cca2 === countryCode)
-// 			console.log(country.name.common)
-// 	} else if (e.target.readyState === 4) {
-// 			console.log('Unable to fetch data')
-// 	}
-
-// })
-
-// countryRequest.open('GET', 'https://restcountries.com/v3.1/all')
-// countryRequest.send()
--- requests.js
+++ requests.js
@@ -1,35 +1,32 @@
-const getPuzzle = (wordCount,  callback) => {
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
   const request = new XMLHttpRequest()
 
   request.addEventListener('readystatechange', (e) => {
     if (e.target.readyState === 4 && e.target.status === 200) {
       const data = JSON.parse(e.target.responseText)
-      callback(undefined, data.puzzle)
+      resolve(data.puzzle)
     } else if (e.target.readyState === 4) {
-      callback('Unable to fetch data', undefined)
+      reject('An error has taken place')
     }
   })
   request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
-}
+})
 
-const getCountry = (countryCode, callback) => {
-  
+const getCountry = (countryCode) =>  new Promise((resolve, reject) => {
+
   const countryRequest = new XMLHttpRequest()
-
   countryRequest.addEventListener('readystatechange', (e) => { 
-    
-    if(e.target.readyState === 4 && e.target.status === 200){
 
+    if(e.target.readyState === 4 && e.target.status === 200){
       const data = JSON.parse(e.target.responseText)
       const country = data.find((country) => country.cca2 === countryCode)
-      callback(undefined, country)
+      resolve(country)
     } else if (e.target.readyState === 4) {
-      callback('Unavle to fetch data')
+      reject('Unable to fetch data')
     }
-
   })
 
   countryRequest.open('GET', 'https://restcountries.com/v3.1/all')
   countryRequest.send()
-}
+})
